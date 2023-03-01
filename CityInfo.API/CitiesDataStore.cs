bef47dd2f4992052a81d9e48a4afe6cb66078ddc
ayoub errazki <ayouberrazki@outlook.com>
2023-03-01 23:08:13 +0100

    Add project files.

---
+++ CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,24 @@
+﻿using CityInfo.API.Models;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        public List<CityDto> Cities { get; set; }
+
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+
+        public CitiesDataStore()
+        {
+            //Init dummy data
+            Cities = new List<CityDto>()
+        {
+            new CityDto(){ Id=1,Name="Al Andalus", Description="Al andalus city where the golden age rised" },
+            new CityDto(){ Id=2,Name="Fès", Description="City full of more than 12 centuries of history" },
+            new CityDto(){ Id=3,Name="Rabat", Description="The capital city of Morocco" },
+            new CityDto(){ Id=4,Name="Marrakesh", Description="The exotic city full of history" }
+
+        };
+        }
+    }
+}
---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,29 @@
+﻿using CityInfo.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(CitiesDataStore.Current.Cities);
+
+        }
+
+        [HttpGet("{Id}")]
+        public ActionResult<CityDto> GetCity(int id)
+        {
+
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id);
+
+            if (city is null)
+                return NotFound();
+            return Ok(city);
+        }
+
+    }
+}
